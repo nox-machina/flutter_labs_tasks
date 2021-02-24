54b3d56e5fbbf2340c27b2da275d15a168c2b66f
0xtheMachine <aaronvsummers@gmail.com>
2021-02-24 23:33:43 +0000

    dynamic task list

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatefulWidget {
-  @override
-  _TaskTileState createState() => _TaskTileState();
-}
+class TaskTile extends StatelessWidget {
+//   @override
+//   _TaskTileState createState() => _TaskTileState();
+// }
 
-class _TaskTileState extends State<TaskTile> {
-  bool isChecked = false;
+// class _TaskTileState extends State<TaskTile> {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
 
-  void toggleCheckState(val) {
-    setState(() {
-      isChecked = val;
-    });
-  }
+  TaskTile({this.taskTitle, this.isChecked, this.checkboxCallback});
+
+  // void toggleCheckState(val) {
+  //   setState(() {
+  //     isChecked = val;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
       contentPadding: EdgeInsets.fromLTRB(30, 0, 30, 0),
       title: Text(
-        "Task #01",
+        taskTitle,
         style: TextStyle(
             decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
       subtitle: Text("Do this pls"),
       value: isChecked,
-      onChanged: (val) {
-        toggleCheckState(val);
-      },
+      onChanged: checkboxCallback,
       secondary: Padding(
         child: Icon(Icons.view_compact_outlined),
         padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import './task_tile.dart';
+import '../models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(title: "Task #01", isDone: false),
+    Task(title: "Task #02", isDone: false),
+    Task(title: "Task #03", isDone: false),
+    Task(title: "Task #04", isDone: false),
+    Task(title: "Task #05", isDone: false),
+    Task(title: "Task #06", isDone: false)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -12,22 +27,19 @@ class TasksList extends StatelessWidget {
         // wrapping in MediaQuery.removePadding to enforce custom listView padding values
         child: MediaQuery.removePadding(
           context: context,
-          child: ListView(
-            children: [
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile(),
-              TaskTile()
-            ],
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return TaskTile(
+                taskTitle: tasks[index].title,
+                isChecked: tasks[index].isDone,
+                checkboxCallback: (bool cbState) {
+                  setState(() {
+                    tasks[index].toggleDone();
+                  });
+                },
+              );
+            },
+            itemCount: tasks.length,
           ),
           removeTop: true,
         ),
